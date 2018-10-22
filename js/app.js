0efbaa08a9845687e5b7559aa3f17148dbba80d6
GauriTikekar <gauri.tikekar@summitaustralia.com.au>
2018-10-22 16:35:58 +1100

    Controllers Views and Route providers added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-var app = angular.module("GroceryListApp", []);
+var app = angular.module("GroceryListApp", ["ngRoute"]);
+
+// Route provider
+app.config(function ($routeProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "views/groceryList.html", 
+			controller: "GroceryListItemsCtrl"
+		})
+		.when("/addItem", {
+			templateUrl: "views/addItem.html", 
+			controller: "GroceryListItemsCtrl"
+		})
+		.otherwise({
+			redirectTo:"/"
+		})
+});
 
 //Grocery list Home controller for the whole page
 app.controller("HomeCtrl", ["$scope", function($scope){
@@ -6,8 +22,8 @@ app.controller("HomeCtrl", ["$scope", function($scope){
 }]);
 
 //Grocery list items controller for the list of items
-app.controller("GroceryListItemsCtrl", ["$scope", function(scope){
-	$scope.items = [{completed: false, name:"Sugar"},
-					{completed: false, name:"Tea"},
-					{completed: false, name:"Coffee"}]
+app.controller("GroceryListItemsCtrl", ["$scope", function($scope){
+	$scope.items = [{completed: false, name:"Sugar", date: "2018-09-14"},
+					{completed: false, name:"Tea", date: "2018-09-15"},
+					{completed: false, name:"Coffee", date: "2018-09-18"}]
 }]);
