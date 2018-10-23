24c3bd88e52f2f395433c7746ad7948e1f311e9e
GauriTikekar <gauri.tikekar@summitaustralia.com.au>
2018-10-23 17:17:03 +1100

    Services added. Add Edit Items functionality added

--- js/app.js
+++ js/app.js
@@ -5,25 +5,95 @@ app.config(function ($routeProvider){
 	$routeProvider
 		.when("/", {
 			templateUrl: "views/groceryList.html", 
-			controller: "GroceryListItemsCtrl"
+			controller: "HomeCtrl"
 		})
 		.when("/addItem", {
 			templateUrl: "views/addItem.html", 
-			controller: "GroceryListItemsCtrl"
+			controller: "GroceryListItemCtrl"
+		})
+		.when("/addItem/edit/:id", {
+			templateUrl: "views/addItem.html", 
+			controller: "GroceryListItemCtrl"
 		})
 		.otherwise({
 			redirectTo:"/"
 		})
 });
 
+// services
+app.service("GroceryService", function (){
+	var groceryService = {};
+	groceryService.groceryItems = [
+		{id: 1, completed: true, itemName: 'Sugar', date:'2018-09-14'},
+		{id: 2, completed: true, itemName: 'Milk', date:'2018-09-14'},
+		{id: 3, completed: true, itemName: 'Tea', date:'2018-09-15'},
+		{id: 4, completed: true, itemName: 'Coffee', date:'2018-09-18'},
+		{id: 5, completed: true, itemName: 'Bread', date:'2018-09-18'},
+	];
+
+// get grocery item by it's id
+	groceryService.findById = function (data) {
+		for (var item in groceryService.groceryItems) {
+			if(groceryService.groceryItems[item].id === data){
+				return groceryService.groceryItems[item];
+			}
+		}
+	}
+
+// get new id for the new grocery item
+	groceryService.getNewId = function (){
+	 	if(groceryService.newId){
+	 		groceryService.newId++;
+	 		return groceryService.newId;
+	 	}
+	 	else {
+	 		var maxId = _.max(groceryService.groceryItems, function(data){ return data.id;})
+	 		groceryService.newId = maxId.id + 1;
+	 		return groceryService.newId;
+ 		}
+	 };
+
+// add or update the grocery item
+	groceryService.save = function (data) {
+		var updatedItem = groceryService.findById(data.id);
+		if (updatedItem) {
+			updatedItem.itemName = data.itemName;
+			updatedItem.completed = data.completed;
+			updatedItem.date = data.date;
+		}
+		else {
+			data.id = groceryService.getNewId();
+			groceryService.groceryItems.push(data);
+		}
+	};
+
+
+	return groceryService;
+});
+
+
 //Grocery list Home controller for the whole page
-app.controller("HomeCtrl", ["$scope", function($scope){
+app.controller("HomeCtrl", ["$scope", "GroceryService", function($scope, GroceryService){
 	$scope.appTitle = "Grocery List";
+	$scope.groceryItems = GroceryService.groceryItems;
 }]);
 
 //Grocery list items controller for the list of items
-app.controller("GroceryListItemsCtrl", ["$scope", function($scope){
-	$scope.items = [{completed: false, name:"Sugar", date: "2018-09-14"},
-					{completed: false, name:"Tea", date: "2018-09-15"},
-					{completed: false, name:"Coffee", date: "2018-09-18"}]
+app.controller("GroceryListItemCtrl", ["$scope", "$routeParams", "$location", "GroceryService", 
+								function($scope, $routeParams, $location, GroceryService){
+
+		// add mew item
+		if($routeParams.id == null){	
+			$scope.groceryItem = {id:0, itemName:'Butter', completed: true, date: new Date()};
+		}
+		// edit existing item
+		else{
+			$scope.groceryItem	= _.clone(GroceryService.findById(parseInt($routeParams.id)));
+		}
+
+		$scope.save = function () {
+			GroceryService.save($scope.groceryItem);
+			$location.path("/");
+		}
 }]);
+
